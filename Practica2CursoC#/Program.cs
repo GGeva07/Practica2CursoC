f146719870da04917b27108f9d265dbbcc2c9b64
evaGO <evaGO@CHIKIBABY>
2025-02-26 14:28:47 -0400

    Add project files.

---
+++ Practica2CursoC#/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practica2CursoC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+            Console.WriteLine("Ingrese 10 numeros");
+            while (numeros.Count != 10)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                numeros.Add(num);
+            }
+            numeros.Sort();
+            Console.Clear();
+            Console.WriteLine($"Lista Ordenada:");
+            foreach(int num in numeros)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
